145084144a9902219aa4affffd7dd91405f8ecbc
aajwalden <aajwalden@gmail.com>
2020-02-05 02:55:48 -1000

    Before workshop 3

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderCampsite({campsiteinfo}) {
     return (
@@ -7,7 +8,6 @@ function RenderCampsite({campsiteinfo}) {
             <Card>
                 <CardImg top src={campsiteinfo.image} alt={campsiteinfo.name} />
                 <CardBody>
-                    <CardTitle>{campsiteinfo.name}</CardTitle>
                     <CardText>{campsiteinfo.description}</CardText>
                 </CardBody>
             </Card>
@@ -24,7 +24,7 @@ function RenderComments({comments}) {
                     return( 
                         <div key={comment.id}>
                             <p>{comment.text}<br/>
-                            --{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                                -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
                         </p>
                         </div>
                         );
@@ -40,8 +40,18 @@ function CampsiteInfo(props) {
         return (
             <div className="container">
                 <div className="row">
+                    <div className="col">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <h2>{props.campsite.name}</h2>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
                     <RenderCampsite campsiteinfo={props.campsite} />
-                    <RenderComments comments={props.campsite.comments} />
+                    <RenderComments comments={props.comments} />
                 </div>
             </div>
         );
